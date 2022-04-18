40be4e37586ea0d18e11666158125198a5a9e14c
khanalbishal <bshlkhnl111@gmail.com>
2022-04-18 21:41:20 +0545

    Done

--- emplyoee-management/src/main/java/com/task/management/scheduler/CalculatingBonus.java
+++ emplyoee-management/src/main/java/com/task/management/scheduler/CalculatingBonus.java
@@ -39,23 +39,6 @@ public class CalculatingBonus {
             }
 
         });
-        List<Employee> employees1 = employeeRepository.getExperinceBetween2And4();
-        employees1.forEach(s -> {
-            if (s.getBonusDistributedDate() != null) {
-                LocalDate date = s.getBonusDistributedDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-                if (date.isBefore(currentDateMinus3Months)) {
-                    Double bonus = s.getSalary() + 15/ 100 * s.getSalary();
-                    Double salaryWithBonus = s.getSalary() + bonus;
-                    employeeRepository.updateSalary(salaryWithBonus, new Date(), s.getId(), bonus);
-                }
-
-            } else {
-                double bonus = s.getSalary() + 15/ 100 * s.getSalary();
-                double salaryWithBonus = s.getSalary() + bonus;
-                employeeRepository.updateSalary(salaryWithBonus, new Date(), s.getId(), bonus);
-            }
-
-        });
         List<Employee> employees2 = employeeRepository.getExperinceBetween4And6();
         employees1.forEach(s -> {
             if (s.getBonusDistributedDate() != null) {
